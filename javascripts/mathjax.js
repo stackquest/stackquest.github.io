f5c7573cce12abf95a690d7355d26e660e9fff79
stackquest <arunksoman@yandex.com>
2022-01-12 22:39:41 +0530

    first commit

---
+++ javascripts/mathjax.js
@@ -0,0 +1,21 @@
+window.MathJax = {
+    loader: {
+      load: ['[tex]/color']
+    },
+    tex: {
+      inlineMath: [["\\(", "\\)"]],
+      displayMath: [["\\[", "\\]"]],
+      processEscapes: true,
+      processEnvironments: true,
+      packages: {'[+]': ['color']}
+    },
+    options: {
+      ignoreHtmlClass: ".*|",
+      processHtmlClass: "arithmatex"
+    }
+  };
+  
+  document$.subscribe(() => { 
+  
+    MathJax.typesetPromise()
+  })
